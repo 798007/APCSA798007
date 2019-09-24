d1d59241226298763f913ff3cfb52e5a94027753
Emily Greene <798007@seq.org>
2019-09-23 18:24:31 -0700

    923

--- Labs/Lab920SentenceBuilder/Runner.java
+++ Labs/Lab920SentenceBuilder/Runner.java
@@ -2,32 +2,13 @@
 /**
  * Write a description of class Runner here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (Emily Greene)
+ * @version (920)
  */
 public class Runner
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Runner
-     */
-    public Runner()
-    {
-        // initialise instance variables
-        x = 0;
+    public static void main(){
+        SentenceBuilder s = new SentenceBuilder();
+        s.loadSentences();
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
     }
-}
--- Labs/Lab920SentenceBuilder/SentenceBuilder.java
+++ Labs/Lab920SentenceBuilder/SentenceBuilder.java
@@ -7,30 +7,54 @@
  */
 public class SentenceBuilder
 {
-    String[] nouns = {"soccer ball", "elephant", "monkey", "Mr. Ettlin", "baseball", "beach", "boy", "girl", "dog", "cat"};
-    String[] verbs = {"steals", "runs", "screams", "jumps", "races", "high-fives", "sings", "swims", "plays", "cooks"};
+    String[] nouns = {"soccer ball", "elephant", "monkey", "Mr. Ettlin", "baseball", "beach", "boy", "girl", "dog", "cat", "Lamborghini", "classroom", "computer", "toaster", "dragon"};
+    String[] verbs = {"steals", "runs", "screams", "jumps", "races", "high-fives", "sings", "swims", "plays", "cooks", "attacks", "sleeps", "drinks", "taunts", "drives"};
     String[] adjectives = {"quiet", "chubby", "angry", "tiny", "unkempt", "loud", "annoying", "strong", "exciting", "magnificent", "athletic", "speedy", "hilarious", "smart"};
-    String[] determiners = {"his", "hers", "their", "Mr. Malloy's", "our", "your", "its", "Mrs. Burbank's", "Isabelle's", "your neighbor's", "Will's", "this", "a", "an", "my", "Bob's", "Mrs. Porter's", "Elon Musk's", "the"};
-    String[] adverbs = {"quickly", "boldly", "excitedly", "carelessly", "gently", "awkwardly", "energetically", "kindly", "frightfully", "gracefully", "happily", "afterwards", "today", "beforehand"};
+    String[] determiners = {"his", "her", "their", "Mr. Malloy's", "our", "your", "its", "Mrs. Burbank's", "Isabelle's", "your neighbor's", "Will's", "this", "a", "my", "Bob's", "Mrs. Porter's", "Elon Musk's", "the"};
+    String[] adverbs = {"quickly", "boldly", "excitedly", "carelessly", "gently", "awkwardly", "energetically", "kindly", "frightfully", "gracefully", "happily", "accidentally", "elegantly", "deliberately", "eventually", "foolishly", "obnoxiously", "repeatedly"};
     String[] prepositions = {"over", "under", "near", "behind", "above", "next to", "on", "below", "past", "before", "beside", "across", "outside", "around", "inside"};
-    public SentenceBuilder()
-    {
-
-    }
+    //prints out a sentence
     public void loadSentences(){
-        
-        
-      
+        System.out.println(getSentence());
     }
-    //public String getSetence(){}
-    
+    //makes a sentence with a noun, verb, and prepositional phrase
+    public String getSentence(){
+        return(getNounPhrase() + " " + getVerbPhrase() + " " + getPrepositionalPhrase());
+    }
+    //returns a noun phrase
     public String getNounPhrase(){
-        return(determiners[(int)(Math.random()*19)] + " " + adjectives[(int)(Math.random()*14)] + " " + nouns[(int)(Math.random()*10)]);
+        return(getRandomDeterminer() + " " + getRandomAdjective() + " " + getRandomNoun());
     }
+    //returns a verb phrase
+    public String getVerbPhrase(){
+        return(getRandomAdverb() + " " + getRandomVerb());
+    }
+    //returns a prepositional phrase
     public String getPrepositionalPhrase(){
-        return(adverbs[(int)(Math.random()*14)] + " " + verbs[(int)(Math.random()*10)]);
+        return(getRandomPreposition() + " " + getRandomDeterminer() + " " + getRandomAdjective() + " " + getRandomNoun());
     }
-    //public String getVerbPhrase(){
-       // return(prepositions[(int)(Math.random()*15)] + " " + determiners[(int)(Math.random()*19)] + " " + adjective
-    //}
+    //returns a random noun from the nouns array
+    public String getRandomNoun(){
+        return(nouns[(int)(Math.random()*15)]);
+    }
+    //returns a random verb from the verbs array
+    public String getRandomVerb(){
+        return(verbs[(int)(Math.random()*15)]);
+    }
+    //returns a random adjective from the adjectives array
+    public String getRandomAdjective(){
+        return(adjectives[(int)(Math.random()*14)]);
+    }
+    //returns a random adverb from the adverbs array
+    public String getRandomAdverb(){
+        return(adverbs[(int)(Math.random()*18)]);
+    }
+    //returns a random determiner from the determiners array
+    public String getRandomDeterminer(){
+        return(determiners[(int)(Math.random()*18)]);
+    }
+    //returns a random preposition from the prepositions array
+    public String getRandomPreposition(){
+        return(prepositions[(int)(Math.random()*15)]);
+    }
 }
