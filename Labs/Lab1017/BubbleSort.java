91e99f726b5dbf5e110d02caa4d77f687dedafda
Emily Greene <798007@seq.org>
2019-10-20 18:51:34 -0700

    1020

--- Labs/Lab1017/BubbleSort.java
+++ Labs/Lab1017/BubbleSort.java
@@ -8,39 +8,35 @@ import java.util.*;
 public class BubbleSort
 {
     static ArrayList<Integer> nums = new ArrayList<Integer>();
-    
     public BubbleSort()
     {
+    }
+    public static void main(){
         loadList(100);
         sortList(nums);
     }
-    
+    //creates an arraylist of numbers between 1 and 100
     public static void loadList(int n){
         for(int i = 0; i < n; i++){
             int random = (int)((Math.random()*100)+1);
             nums.add(random);
         }
     }
-    
+    //sorts the arraylist from least to greatest and prints the sorted arraylist
     public static void sortList(ArrayList<Integer> list){
-        for(int i = 0; i < list.size(); i++){
-            for(int j = 0; j < list.size()-j; j++){
-                if(list.get(j) > list.get(j+1)){
-                    swap(list, j, j+1);
+        for(int i = list.size()-1; i >= 0; i--){
+            for(int j = 1; j <= i; j++){
+                if(list.get(j-1) > list.get(j)){
+                    swap(list, j-1, j);
                 }
             }
         }
         System.out.print(list);
     }
-    
+    //swaps two values in the arraylist if the next integer is greater than the first integer
     public static void swap(ArrayList<Integer> ints, int i, int n){
         int temp = ints.get(i);
         ints.set(i, n);
         ints.set(n, temp);
-    }
-
-    public static void main(){
-        loadList(100);
-        sortList(nums);
     }
 }
